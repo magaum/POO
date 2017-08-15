0adfed679cefc8144845e0a6d05fcce134d6fa60
weslei luiz <weslei_luiz22@hotmail.com>
2017-08-15 10:53:42 -0300

    Create Rfor.java

---
+++ Rfor.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Rfor {
+
+	public static void main(String[] args) {
+		System.out.println("**********");
+		for(int i = 8;i>0;i--)
+			System.out.println("*        *");
+		System.out.println("**********");
+	}
+}
