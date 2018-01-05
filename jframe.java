d8513957f70cf3a77d37567c138f74238c42f58f
weslei luiz <weslei_luiz22@hotmail.com>
2018-01-05 02:52:28 -0200

    Add files via upload

---
+++ jframe.java
@@ -0,0 +1,15 @@
+package br.com.fatec;
+
+public class jframe {
+	public static void main(String args[]) {
+		/*Ex1 ex1 = new Ex1();*/
+		Ex2 ex2 = new Ex2();
+		//Instancia a tela com o titulo entre aspas
+		//JFrame tela = new JFrame("Ex1");
+		//tela.getContentPane().add(ex1);
+		//tela.setSize(800,400);
+		//tela.getContentPane().add(ex2);
+		//tela.setVisible(true);
+		
+	}
+}
